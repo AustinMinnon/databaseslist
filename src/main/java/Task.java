b6265e16f8f17e14fc2b423ee44b9f72096015e3
Austin Minnon and Kyle Derrick <kderrick1988@gmail.com>
2016-02-22 11:58:05 -0800

    add specs for dbs

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,55 +1,114 @@
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
 
 public class Task {
-  private static ArrayList<Task> instances = new ArrayList<Task>();
+  private int id;
+  private String description;
 
-  private String mDescription;
-  private LocalDateTime mCreatedAt;
-  private boolean mCompleted;
-  private int mId;
-
-  public Task (String description) {
-    mDescription = description;
-    mCreatedAt = LocalDateTime.now();
-    mCompleted = false;
-    instances.add(this);
-    mId = instances.size();
+  public int getId() {
+    return id;
   }
 
   public String getDescription() {
-    return mDescription;
+    return description;
   }
 
-  public boolean isCompleted() {
-    return mCompleted;
+  public Task(String description) {
+    this.description = description;
   }
 
-  public LocalDateTime getCreatedAt() {
-    return mCreatedAt;
-  }
-
-  public int getId() {
-    return mId;
-  }
-
-  public void completeTask() {
-    mCompleted = true;
-  }
-
-  public static ArrayList<Task> all() {
-    return instances;
-  }
-
-  public static Task find(int id) {
-    try {
-      return instances.get(id - 1);
-    } catch (IndexOutOfBoundsException e) {
-      return null;
+  @Override
+  public boolean equals(Object otherTask){
+    if (!(otherTask instanceof Task)) {
+      return false;
+    } else {
+      Task newTask = (Task) otherTask;
+      return this.getDescription().equals(newTask.getDescription()) &&
+            this.getId() == newTask.getId();
     }
   }
 
-  public static void clear() {
-    instances.clear();
+  public static List<Task> all() {
+    String sql = "SELECT id, description FROM Tasks";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Task.class);
+    }
   }
-}
+
+  public void save() {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "INSERT INTO Tasks (description) VALUES (:description)";
+    this.id = (int) con.createQuery(sql, true)
+      .addParameter("description", this.description)
+      .executeUpdate()
+      .getKey();
+  }
+  }
+  public static Task find(int id) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "SELECT * FROM Tasks where id=:id";
+    Task task = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Task.class);
+    return task;
+  }
+  }
+ }
+
+
+
+// import java.time.LocalDateTime;
+// import java.util.ArrayList;
+//
+// public class Task {
+//   private static ArrayList<Task> instances = new ArrayList<Task>();
+//
+//   private String mDescription;
+//   private LocalDateTime mCreatedAt;
+//   private boolean mCompleted;
+//   private int mId;
+//
+//   public Task (String description) {
+//     mDescription = description;
+//     mCreatedAt = LocalDateTime.now();
+//     mCompleted = false;
+//     instances.add(this);
+//     mId = instances.size();
+//   }
+//
+//   public String getDescription() {
+//     return mDescription;
+//   }
+//
+//   public boolean isCompleted() {
+//     return mCompleted;
+//   }
+//
+//   public LocalDateTime getCreatedAt() {
+//     return mCreatedAt;
+//   }
+//
+//   public int getId() {
+//     return mId;
+//   }
+//
+//   public void completeTask() {
+//     mCompleted = true;
+//   }
+//
+//   public static ArrayList<Task> all() {
+//     return instances;
+//   }
+//
+//   public static Task find(int id) {
+//     try {
+//       return instances.get(id - 1);
+//     } catch (IndexOutOfBoundsException e) {
+//       return null;
+//     }
+//   }
+//
+//   public static void clear() {
+//     instances.clear();
+//   }
+// }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -3,12 +3,12 @@ import org.sql2o.*;
 
 public class DatabaseRule extends ExternalResource {
 
-  protect void before() {
+  protected void before() {
     DB.sql2o = new Sql2o ("jdbc:postgresql://localhost:5432/to_do_test", null, null);
   }
 
   protected void after() {
-    try(Connection con = DM.sql2o.open()) {
+    try(Connection con = DB.sql2o.open()) {
       String deleteTasksQuery = "DELETE FROM tasks *;";
       con.createQuery(deleteTasksQuery).executeUpdate();
     }
